07a8aae572db057082df690214649bdc06a93cad
Kseniya <kseniya-ignateva-2002@mail.ru>
2022-09-18 19:53:37 +0500

    New posts are created and displayed on the main page

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -7,8 +7,9 @@ public class Post {
     private Integer likes;
     private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
     }
     public Date getCreationDate() {
         return creationDate;
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -2,32 +2,25 @@ package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Random;
 
 @Service
 public class PostService {
+    ArrayList<Post> postArrayList = new ArrayList<>(Arrays.asList(
+            new Post("HELP...", new Date()),
+            new Post("CAN U HELP ME", new Date()),
+            new Post("OH MY GOD.", new Date())));
+
     public ArrayList<Post> listAllPosts() {
-        Post post1 = new Post("HELP...");
-        Post post2 = new Post("CAN U HELP ME?");
-        Post post3 = new Post("OH MY GOD.");
-
-        ArrayList<Post> postArrayList = new ArrayList<>();
-
-        postArrayList.add(post1);
-        postArrayList.add(post2);
-        postArrayList.add(post3);
-
-        Random random = new Random();
-
-        for (Post post : postArrayList) {
-            post.setLikes(Math.abs(random.nextInt(1000)));
-        }
-
         return postArrayList;
+    }
+
+    public void create(String text) {
+        Post post = new Post(text, new Date());
+        postArrayList.add(post);
     }
 }
