ca919680b3aaed49eb2aa76cce42f22bf5c0b71f
Kseniya <kseniya-ignateva-2002@mail.ru>
2022-09-18 19:13:26 +0500

    Bootstrap вёрстка

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,11 +1,17 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
     public Post(String text) {
         this.text = text;
+    }
+    public Date getCreationDate() {
+        return creationDate;
     }
 
     public String getText() {
